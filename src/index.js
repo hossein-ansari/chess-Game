2f1feba30ccc411af0d175eb62e99f8cbff86828
hossein <hossein838485@gmail.com>
2023-08-09 12:01:43 +0330

    add

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter  } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import {AllDatasProvider} from "./context/context";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
